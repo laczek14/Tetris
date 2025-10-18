747036f8dc7e83c2d1fb1c7558a537f6b6fdcad2
laczek14 <hubercik944@gmail.com>
2025-10-18 19:05:46 +0200

    changes

--- Game.cs
+++ Game.cs
@@ -1,13 +1,16 @@
-﻿using Raylib_cs;
+﻿using System.Diagnostics;
+using Raylib_cs;
 using System.Numerics;
+
 namespace tetris
 {
-    internal class Tetris 
+    internal class Tetris
     {
         public const int Width = 480;
         public const int Height = 480;
         public int pixel = 40;
         public List<TetroManager> TetroHold = new List<TetroManager>();
+
         public void Start()
         {
             Raylib.InitWindow(Width, Height, "tetris");
@@ -15,26 +18,28 @@ namespace tetris
             {
                 Render();
             }
+
             Raylib.WindowShouldClose();
         }
 
-        void Render() 
+        void Render()
         {
             Raylib.BeginDrawing();
             Grid();
             foreach (TetroManager tetroholds in TetroHold)
             {
-                Pixel(tetroholds);   
+                Pixel(tetroholds);
             }
+
             Raylib.ClearBackground(Color.White);
             Raylib.EndDrawing();
         }
 
-        void Pixel(TetroManager Info = new TetroManager())
+        void Pixel(TetroManager TetroManager = new TetroManager())
         {
-            Color Color = Info.Color;
-            Vector2 Position = new Vector2(Info.Position.X, Info.Position.Y);
-            bool IsVisible = Info.IsVisible;
+            Color Color = TetroManager.Color;
+            Vector2 Position = new Vector2(TetroManager.Position.X, TetroManager.Position.Y);
+            bool IsVisible = TetroManager.IsVisible;
             Vector2 V = new Vector2(pixel, pixel);
             Raylib.DrawRectangleV(Position, V, Color);
         }
@@ -76,16 +81,16 @@ namespace tetris
                 Vector2 pos2 = new Vector2(pos.X, pos.Y + pixel);
                 Vector2 pos3 = new Vector2(pos.X, pos2.Y + pixel);
                 Vector2 pos4 = new Vector2(pos.X + pixel, pos3.Y);
-               TetroHold.Add(new TetroManager(color,pos,isVisible1,isActive));
-               TetroHold.Add(new TetroManager(color,pos2,isVisible2,isActive));
-               TetroHold.Add(new TetroManager(color,pos3,isVisible3,isActive));
-               TetroHold.Add(new TetroManager(color,pos4,isVisible4,isActive));
+                TetroHold.Add(new TetroManager(color, pos, isVisible1, isActive));
+                TetroHold.Add(new TetroManager(color, pos2, isVisible2));
+                TetroHold.Add(new TetroManager(color, pos3, isVisible3));
+                TetroHold.Add(new TetroManager(color, pos4, isVisible4));
             }
         }
 
         public struct TetroManager
         {
-            public TetroManager(Color color,Vector2 position,bool isVisible, bool isActive)
+            public TetroManager(Color color, Vector2 position, bool isVisible, bool isActive)
             {
                 Color = color;
                 Position = position;
@@ -93,10 +98,36 @@ namespace tetris
                 IsActive = isActive;
             }
 
-            public Color Color{get;}
+            public TetroManager(Color color, Vector2 position, bool isVisible)
+            {
+                Color = color;
+                Position = position;
+                IsVisible = isVisible;
+            }
+
+
+
+            public Color Color { get; }
             public Vector2 Position { get; }
             public bool IsVisible { get; }
             public bool IsActive { get; }
+        }
+
+        void Movement()
+        {
+            foreach (var var in TetroHold)
+            {
+                switch (var.IsActive)
+                {
+                    case true:
+                        if (Raylib.IsKeyPressed(KeyboardKey.D) & var.Position.X > -0)
+                        {
+                            var.Position.X + pixel * Raylib.GetFrameTime();
+                        }
+
+                        break;
+                }
+            }
         }
     }
 }
